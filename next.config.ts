a982661b7925f504c6e162d4716f3751d9525b61
Manjunath <dmanjunath@gmail.com>
2025-09-18 15:43:46 +0530

    Enhance project configuration and layout: Added image domain support in next.config.ts, updated package dependencies, and restructured the main page layout with new components for improved UI.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["ik.imagekit.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
