9cb06026c738916f8468d095ff51277d5b411f7d
Manjunath <dmanjunath@gmail.com>
2025-09-19 19:40:38 +0530

    Refactor Home page and Footer component: Removed unused imports from Home page for a cleaner structure and streamlined the Footer component by commenting out the Logo import.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,4 @@
-import CallToAction from "@/components/call-to-action";
-import ContentSection from "@/components/content-2";
-import Features from "@/components/features-3";
-import FooterSection from "@/components/footer";
 import HeroSection from "@/components/hero-section";
-import IntegrationsSection from "@/components/integrations-7";
-import StatsSection from "@/components/stats";
 
 export default function Home() {
   return (
